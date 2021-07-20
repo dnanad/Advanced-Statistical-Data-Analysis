17aa4d67753373a43d8c6a9b494040c1c9fd80d7
Anand Deshpande <anand.deshpande@stud.uni-goettingen.de>
2021-07-20 23:07:41 +0200

    ex2

--- ex2/ex2.R
+++ ex2/ex2.R
@@ -1,14 +1,16 @@
-### ex 2 ###
+  ### ex 2 ###
 
 
-### preamble ###
+  ### preamble ###
 library(xtable)
 library("tidyverse")
 
-### (a) ###
-#Switch the default random number generator in R to Wichmann-Hill. 
 
-#?Random
+
+  ### (a) ###
+  #Switch the default random number generator in R to Wichmann-Hill. 
+
+?Random
 
 RNGkind('Wichmann-Hill')
 set.seed(143, 'Wichmann-Hill')
@@ -21,13 +23,33 @@ p <- 0.4
 
 
 
-#inversion method, 
-#by simulating corresponding Bernoulli random variables 
-#by inversion method
-#by using R function rbinom. 
+##method 1: by inversion method, 
+# Let us generate [0,1]-uniform r.v.
+uni_rv <- runif(N)
+binom_inversion <- function(X,n,p) {
+  y= NULL
+  for (k in 1:length(X)){
+    x = X[k]
+    for (l in 10:0) {if (x<=pbinom(l,n,p)) y[k]=l}
+  }
+  return(y)
+}
+bin1 <- binom_inversion(uni_rv,n,p)
+
+## method 2: by simulating corresponding Bernoulli random variables by inversion method
+# Let us produce n*N uniform r.v. and transform them
+uni_rv2 <- runif(n*N)
+Bern <- matrix(as.numeric(uni_rv2<p), nrow = N)
+# then add up the independent bernoullis to get binomials
+bin2 <- rowSums(Bern)
+
+## method 3: by using R function rbinom
+bin3 <- rbinom(N, n, p)
+
 
 
 
 #Plot the empirical probability density functions of all three samples on one panel. 
+
 #Comment on the results. 
 #Switch the random number generator back to its default.
